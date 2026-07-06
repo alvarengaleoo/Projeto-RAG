465a9abc6e567e17d493384cf4ea04253ee93a93
Leonardo Alvarenga <alvarengaleoo@gmail.com>
2026-07-06 13:17:17 -0300

    carregamento de documentos PDF, DOCX e TXT

---
+++ utils/loaders.py
@@ -0,0 +1,17 @@
+from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
+from pathlib import Path
+from config import BASE_PATH
+
+
+def carregar_documentos():
+    documentos = []
+    for arquivo in BASE_PATH.glob("*.pdf"):
+        loader = PyPDFLoader(str(arquivo))
+        documentos.extend(loader.load())
+    for arquivo in BASE_PATH.glob("*.docx"):
+        loader = Docx2txtLoader(str(arquivo))
+        documentos.extend(loader.load())
+    for arquivo in BASE_PATH.glob("*.TXT"):
+        loader = TextLoader(str(arquivo), encoding="utf-8")
+    print(f'Total de documentos carregados: {len(documentos)}')
+    return documentos
